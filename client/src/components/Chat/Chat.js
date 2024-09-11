a7eb1b1b9bd5b517ffb4b11de6a5b865f562e6b5
Krishom <83640457+Krish-Om@users.noreply.github.com>
2024-09-11 12:41:32 +0545

    Update chat endpoint URL and package dependencies

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -10,7 +10,7 @@ import Messages from "../Messages/Messages";
 import './Chat.css';
 
 let socket;
-const ENDPOINT = "https://chat-app-simple-1.onrender.com";
+const ENDPOINT =proecess.env.ENDPOINT;
 
 const Chat = ()=>{
     const location = useLocation();
